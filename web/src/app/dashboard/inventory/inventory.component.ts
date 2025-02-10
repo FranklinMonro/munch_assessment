477ddad3566e380cae492a0d23d5a25be91738d6
FranklinMonro <fbg2468@gmail.com>
2025-02-10 12:00:52 +0000

    component service

--- web/src/app/dashboard/inventory/inventory.component.ts
+++ web/src/app/dashboard/inventory/inventory.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { MatDividerModule } from '@angular/material/divider';
+
 @Component({
   selector: 'app-inventory',
-  imports: [],
+  imports: [
+    MatDividerModule
+  ],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.scss'
 })
